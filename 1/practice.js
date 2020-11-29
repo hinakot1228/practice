1d7733b5356ee7b9af772bda2e689e8a1c7176c7
Hinako-Tanaka <youngbird1228@gmail.com>
2020-11-29 23:23:17 +0900

    exercise js

---
+++ 1/practice.js
@@ -0,0 +1,11 @@
+// Q1
+let text1= document.getElementById('text1');
+console.log(text1.textContent);
+
+// Q2
+let text2 = document.getElementById('text2');
+text2.innerHTML = '<p>変更しました</p>';
+
+// Q3
+let text3 = document.getElementById('text3');
+text3.style.color = 'red';
